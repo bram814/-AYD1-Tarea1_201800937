c01260ad9929d8ae943ca63fc109d7c3ac04ca5f
bram814 <abraham81423@gmail.com>
2022-08-09 20:11:57 -0600

    Se agregó calculadora

---
+++ src/routes/Routes.js
@@ -0,0 +1,28 @@
+const {Router} = require('express');
+const router = Router();
+
+// req : request
+// res : responseve
+router.get('/', (req, res) => {
+    console.log("NUESTRO PRIMER ENDPOINT")
+    res.status(200).json({
+        msg:"todo ok"
+    });
+});
+
+
+router.post('/Calculadora-201800937', async(req, res) =>{
+
+    var num1 = req.body.num1;
+    var num2 = req.body.num2;
+    
+    res.status(200).json({
+        numero1:num1,
+        numero2:num2,
+        suma:num1+num2
+
+    });
+})
+
+
+module.exports = router;
